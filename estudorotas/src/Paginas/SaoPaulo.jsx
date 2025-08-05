5c6a86b4175de55961364a1882e3de26b1651615
ad0-dev <isabellyanacleto01@gmail.com>
2025-08-05 14:48:30 -0300

    5/8
    5/8

---
+++ estudorotas/src/Paginas/SaoPaulo.jsx
@@ -0,0 +1,20 @@
+export default function SaoPaulo()
+{
+    return(
+        <div>
+            <h1>Seja bem vindo ao Estado de São Paulo</h1>
+
+            <p>
+                 O estado de São Paulo é o mais populoso do Brasil e um dos principais centros econômicos do país. Sua capital, também chamada São Paulo, é conhecida por sua diversidade cultural e grande importância industrial e financeira. Além disso, o estado possui paisagens variadas, desde áreas urbanas intensas até regiões naturais como a Serra do Mar.
+                </p>
+
+                <p>
+                    <img src="" />
+                </p>
+
+                <p>
+                    <a href="/">Voltar para Home</a>
+                </p>
+        </div>
+    )
+}
