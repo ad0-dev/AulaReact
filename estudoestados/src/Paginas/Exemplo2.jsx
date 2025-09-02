67e7087f1b5fae218b0c97e33afc52415d459324
ad0-dev <isabellyanacleto01@gmail.com>
2025-09-02 14:55:31 -0300

    02/09

--- estudoestados/src/Paginas/Exemplo2.jsx
+++ estudoestados/src/Paginas/Exemplo2.jsx
@@ -3,7 +3,33 @@ export default function Exemplo2() {
     <div>
       <h1>Exemplo 2</h1>
 
-      
+      <div className="conteudo">
+        <h3>Calculo da Média Artimetica</h3>
+
+        <p>
+          O Objetivo aqui é receber o nome do aluno, suas 3 notas e calcular a média
+        </p>
+
+       <form>
+          <p>
+            Nome do Aluno: <br />
+            <input type="text" />
+          </p>
+
+          <p>
+            Nota 1: <br />
+            <input type="text" />
+          </p>
+
+          <p>
+            Nota 2: <br />
+            <input type="text" />
+          </p>
+
+     
+        </form>
+        </div>
+
     </div>
   );
 }
