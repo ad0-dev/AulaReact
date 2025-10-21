feb823b77f7397bb92937cd9c9d60416c009a2b4
ad0-dev <isabellyanacleto01@gmail.com>
2025-10-21 14:50:26 -0300

    Update Exercicio1.jsx
    21/10

--- estudoestados/src/Paginas/Exercicio1.jsx
+++ estudoestados/src/Paginas/Exercicio1.jsx
@@ -1,31 +1,45 @@
+import { useState } from "react";
 export default function Exercicio1()
-{
-    return(
-        <div>
-            <h1>Exercício 1</h1>
+{  
+    const[numero, setNumero] =useState(0);
+    const[resultado, setResultado] =useState();
 
-        <div className="conteudo">
+    function calcular ()
+    {
+    const quadrado = Number * Number;
+    const cubo = Number* Number * Number;}
 
-            <h3>Calculo do Quadrado/Cubo</h3>
-
-            <form>
-
-                <p>
-                    Digite um número qualquer <br/>
-                    <input type="text" />
-                </p>
-
-                <p>
-                    <input type="button" value="Exercício 1" />
-                </p>
-                
-                <p>
-                    <a href="/">Voltar</a>
-                </p>
-
-            </form>
-
-            </div>   
-        </div>
-    )
+    setResultado("O quadrado é "+ quadrado+ "o cubo é"+ cubo);
 }
+    return (
+      <div>
+        <h1>Exercicio 1</h1>
+  
+        <div className="conteudo">
+  
+          <h3>Calculo do Quadrdo/Cubo</h3>
+  
+          <form>
+            <p>
+             Digite um numero qualquer:  <br />
+              <input type="text" value={numero}
+              onChange={(e) => setNumero (e.target.value)} />
+            </p>
+
+            <p>
+              <input type="button" value="Calcular" 
+              onClick={calcular} />
+            </p>
+            <p>
+                <b>
+                    o resultado
+                </b>
+            </p>
+  
+          </form>
+          </div>
+  
+      </div>
+    );
+  }
+  
